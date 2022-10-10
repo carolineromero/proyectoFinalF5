7525907516c7ccaefc48552f9a399157b1a13f2a
Dweina1920 <dina.yamanid@gmail.com>
2022-10-10 12:30:58 +0200

    confirmar ramaDina

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -38,7 +38,7 @@ function LogIn(){
                             </a>
                             <div className="mt-6">
                                 <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
-                                    Login
+                                    Login here
                                 </button>
                             </div>
                         </form>
