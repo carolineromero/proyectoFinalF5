5dfd6b516f660f9941be064c9e126f69e56db905
RABIAZEEM <rabiaazeem48@gmail.com>
2022-10-10 13:06:49 +0200

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'moradoFuerteF5':'#4311B9',
+        'naranjaF5' :'#FF4700',
+        'lilaF5':'#CBBFE8',
+        'cianF5':'#33C8CD',
+        'rositaF5':'#FED2C0',
+      },
+
+    },
   },
   plugins: [],
 }
+
