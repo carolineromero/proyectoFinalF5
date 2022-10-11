868f9b5f00821f56b98b7d0544328c259e2a353a
caroolromero <carolf5laci@gmail.com>
2022-10-11 18:39:56 +0200

    cambio rutas router, pagina home y btn burger

--- src/components/home/Name.jsx
+++ src/components/home/Name.jsx
@@ -4,10 +4,10 @@ function Name() {
   return (
     <>
     <div className="flex justify-center flex-col md ">
-    <h2 className="text-3xl font-bold self-center text-naranjaF5">
+    <h4 className="text-1xl font-bold self-center text-moradoFuerteF5">
       Hola..
-    </h2>  
-    <h2 className="text-3xl font-bold self-center text-naranjaF5 ">
+    </h4>  
+    <h2 className="text-3xl font-bold self-center text-moradoFuerteF5 ">
     Pepa
     </h2> 
     </div>
