b82244f409c06bf0afef15e7ae5e63eb2d340cc8
RABIAZEEM <rabiaazeem48@gmail.com>
2022-10-13 09:07:15 +0200

    component change

--- src/components/ProfileForm.jsx
+++ src/components/ProfileForm.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import image from '../assets/img/perfil.png'
 
 function ProfileForm() {
   return (
     <body>
-    <div className="container columns-1 place-content-center shadow-sm">
+        <div className='ProfileForm w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-rose-600/40 ring-2 ring-moradoFuerteF5 object-fill '>
+    
+    <div className="containerImg w-full p-6 m-auto bg-white rounded-md ring-2 ring-moradoFuerteF5 max-h-full">
+     <img className="h-48 w-full object-cover md:h-full md:w-48 flex items-center rounded-l" src={image} alt=""/>
+    </div>
+    </div>
+    <div className="container columns-1 place-content-center shadow-sm lg:max-w-xxl">
     <form className="mt-6">
+                            <div className="mb-2">
+                                <label
+                                    for="Nombre"
+                                    className="block text-sm font-semibold text-gray-800"
+                                >
+                                    Nombre
+                                </label>
+                                <input
+                                    type="text"
+                                    className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                                />
+                            </div>
                             <div className="mb-2">
                                 <label
                                     for="DNI"
@@ -55,6 +74,18 @@ function ProfileForm() {
                                     className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                                 />
                             </div>
+                            <div className="mb-2">
+                                <label
+                                    for="Horas semanales"
+                                    className="block text-sm font-semibold text-gray-800"
+                                >
+                                    Horas Semanales
+                                </label>
+                                <input
+                                    type="Horas Semanales"
+                                    className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                                />
+                            </div>
                             
                             <div className="mt-6">
                                 <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
@@ -63,8 +94,10 @@ function ProfileForm() {
                             </div>
                         </form>
         
-
+                   
+    
     </div>
+    
 </body>
   )
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import image from '../assets/img/perfil.png'
+// import image from '../assets/img/perfil.png'
 import ProfileForm from '../components/ProfileForm'
 import Header from '../components/Header'
 
@@ -7,47 +7,11 @@ import Header from '../components/Header'
 
 function Profile() {
   return (
-    <div>
-        <Header/>
-<div className='ProfileForm w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-rose-600/40 ring-2 ring-moradoFuerteF5 lg:max-w-xxl '>
-    
-<div className="containerImg w-full p-6 m-auto bg-white rounded-md ring-2 ring-moradoFuerteF5 max-h-full">
- <img className="h-48 w-full object-cover md:h-full md:w-48 flex items-center rounded-l" src={image} alt=""/>
-</div>
-    <ProfileForm/>
-   
-{/*  
-    <div className="body">
-        <div className="Informe de Registro flexsm (640px)shadow-md ">
-    <label for="">Informe de Registro</label>
-                    <input type="text">
-                        </input>
-                        </div>
-
-
-                   <div className="#cbd5e1"> <label for="">DNI</label>
-                    <input type="">
-                        </input></div>
-
-                       <div className="#cbd5e1"> <label for="Telefono">Telefono</label>
-                    <input type="number">
-                        </input></div>
-
-                      <div> <label for="">Email</label>
-                    <input type="text">
-                        </input></div> 
-
-                       <div> <label for="">Cargo</label>
-                    <input type="text">
-                        </input></div>
-                        <button className="btn">Editar</button>
-                </div> */}
-              
-    </div>
-    </div>
-  )
-  
-  
+    <>
+    <Header/>
+    <ProfileForm/>       
+    </>
+  ) 
 }
 
 
