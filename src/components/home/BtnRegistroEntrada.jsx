0018b7fd1f83f9a62e30186f9b954a1e78911c85
Dweina1920 <dina.yamanid@gmail.com>
2022-10-14 12:59:38 +0200

    día y hora en home

--- src/components/home/BtnRegistroEntrada.jsx
+++ src/components/home/BtnRegistroEntrada.jsx
@@ -3,16 +3,19 @@ import IconEdit from '../../assets/img/header/editIcon.png'
 
 
 function BtnRegistroEntrada() {
-  const date = new Date();
-  console.log(date)
-  function convert(str){
-    let date = new Date(str),
-    month= ("0" + (date.getMonth()+1)).slice(-2);
-    day= ("0" + date.getDate()).slice(-2);
+  
+    
+    const date = new Date();
+    const month= ("0" + (date.getMonth()+1)).slice(-2);
+    const day= ("0" + date.getDate()).slice(-2);
     const hour = date.getHours();
     const minutes = date.getMinutes();
+    const year = date.getFullYear();
 
-  }
+   
+
+  
+  
   
 
  
@@ -20,17 +23,17 @@ function BtnRegistroEntrada() {
     <>
    
     <div className="flex justify-center">
-        <h4 className="text-1xl font-bold flex self-center bg-cianF5 w-max p-5 m-4 rounded-2xl text-white">
+        <button className="text-1xl font-bold flex self-center bg-cianF5 w-max p-5 m-4 rounded-2xl text-white" >
           Registrar entrada
-        </h4> 
+        </button> 
 
     </div>
     <div className='flex justify-center'>
         <div className='flex flex-col justify-between   bg-violet-300 p-5 rounded-lg w-200'>
             <div className='flex justify-around '>
                 <div className="bg-green-400 w-14 h-14 rounded-full ml-0.5"> </div>
-                <h2 className="flex self-center text-1xl text-black font-bold "> `${convert(hour)}`:`${convert(minutes)}`</h2>
-                <h2 className="flex self-center text-1xl text-black font-bold "> `${convert(day)}-`${convert(hour)}`-`${convert(date.getFullYear())}`</h2>
+                <h2 className="flex self-center text-1xl text-black font-bold "> {hour}:{minutes}</h2>
+                <h2 className="flex self-center text-1xl text-black font-bold "> {day}-{hour}-{year}</h2>
                 <h2 className="flex self-center text-1xl text-black font-bold "> Entrada </h2>
                 <img src={IconEdit} alt="icono editar" className="w-7 h-7 flex self-center " />
         </div>
