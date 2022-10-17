a3e03c66418a562201782ad70f9ed39d6082f5b0
caroolromero <carolf5laci@gmail.com>
2022-10-17 13:58:22 +0200

    mini cambio en el estilo del background general

--- src/pages/LogInPage.jsx
+++ src/pages/LogInPage.jsx
@@ -10,13 +10,13 @@ function LogInPage(){
        
         <LogIn/>
        
-        {/* <div className='absolute  top-0 -right-4 w-60 h-60 bg-purple-300 opacity-50 z-1 filter blur-3xl rounded-full'></div>
+      <div className='absolute  top-0 -right-4 w-60 h-60 bg-purple-300 opacity-50 z-1 filter blur-3xl rounded-full'></div>
 
         <div className='absolute bottom-0 -left-4 w-60 h-60 bg-grisly2F5 opacity-50 z-1 filter blur-3xl rounded-full'></div>
           
           <img className="absolute content-center -right-10 top-2/4 z-1 w-60 h-60 fiter blur-3xl opacity-50" src={Vector} alt=""/>
           
-         */}
+        
         
 
         
