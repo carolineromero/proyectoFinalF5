0383581a695167617e666a4fbcfc55b2d9650c1e
@gilsusana <gilsusanaCIBAF5@gmail.com>
2022-10-17 14:46:16 +0200

    home cambios importación

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,40 +2,18 @@ import React from 'react';
 import Header from '../components/home/Header';
 import Hero from '../components/home/Hero';
 import Main from '../components/home/Main'
-
-import logoSomosF5 from '../assets/img/header/somosF5-logoMorado.svg'
 import BgMain from '../components/global/BgMain';
 
 
 
 function Home() {
   return (
-<<<<<<< HEAD
-    <>  
-  <Header/>
-  <Hero/>
-  <Main/>
-=======
-    <div className= 'relative'>
-       <BgMain/>
-        <header className='flex justify-between'>
-          <img src={logoSomosF5} alt="" srcset="" />
-          <button>--8</button>
-        </header>
->>>>>>> Carol
-
-        <section>
-          <h2>
-            Hola Isabel
-          </h2>
-        
-        </section>
-
-        <main>
-          <input type="text" />
-        </main>
-
-    </div>
+  <>
+        <BgMain/>
+        <Header/>
+        <Hero/>
+        <Main/>
+  </> 
   )
 }
 
