02f43cc3ca5311e9ac24aca07ba80a92653a1bc0
caroolromero <carolf5laci@gmail.com>
2022-10-20 21:46:25 +0200

    pagina search ok responsive y desktop

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -41,36 +41,32 @@ const Search = () => {
   return (
     <>
 
-      <div className="flex">
-        <form className='text-slate-100 '>
-          <h2 className="relative flex justify-center h-10 text-moradoFuerteF5">
-            <input value={search} onChange={searcher} type="search" id="default-search" className="bg-grisF5 rounded-xl flex content-center" fill=" moradoFuerteF5 " required>
-            </input>
-          </h2>
-        </form>
+<div className= 'flex justify-center grid-cols-2 mx-6'>
+     <input value={search} onChange={searcher} type="search" id="default-search" className="w-60 h-6 flex justify-center items-center bg-grisF5 p-5 m-2 rounded-md border-2 text-moradoFuerteF5" fill=" moradoFuerteF5 " required>
+     </input>
 
-        <div className="flex justify-center">
-          <button className="w-40 h-6 text-1xl flex justify-center items-center bg-moradoFuerteF5  p-5 m-4 rounded-xl border-2 text-white">
-            Crear nuevo
-          </button>
-        </div>
-      </div>
+  <button className='w-30 h-6 flex justify-center items-center  text-xs bg-moradoFuerteF5 p-5 m-2 rounded-md border-2 text-white'>
+     Crear nuevo
+  </button>
+</div>
 
       {results.map((user) => (
 
-        <div class="mx-5 my-2 grid-cols-5 grid-row-2 rounded-lg border-2 border-b-4 border-l-4 border-moradoFuerteF5 px-4 shadow-xl">
+
+        <div className='relative mx-6 lg:mx-80'>
+        <div class="mx-5 my-2 grid-cols-5 grid-row-2 rounded-lg border-2 border-b-4 border-l-4 bg-white border-moradoFuerteF5/20 px-4 shadow-xl">
           <div class="flex h-16 items-center justify-between">
             <a href="http://localhost:3000/profile"><img alt="photo" class="w-10 rounded-full" src={user.image} /></a>
-            <div class="ml-2">
+            <div class="ml-2" >
 
               <div class="text-xs flex font-semibold text-moradoFuerteF5 w-40">{`${user.name} ${user.surname}`} </div>
 
-              <div class="flex text-xs font-light text-gray-600">
+              <div class="flex text-xs font-light font-family: 'Poppins' text-gray-600">
                 {user.cargo}
               </div>
             </div>
             <div className="flex flex-row-reverse w-full relative bottom-0">
-              <div className="grid gap-4 grid-cols-3 border-t divide-x text-[#0ED3CF]  bg-gray-50 dark:bg-transparent py-3">
+              <div className="grid gap-4 grid-cols-3 dark:bg-transparent py-3">
                 <a className=" cursor-pointer uppercase text-xs flex flex-row items-center justify-center font-semibold">
                   <div className="mr-2" >
                     <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 24 24" width="20px" fill="#4311B9"><path d="M0 0h24v24H0z" fill="none" /><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" /></svg>
@@ -93,6 +89,7 @@ const Search = () => {
               </div>
             </div>
           </div>
+        </div>
         </div>
 
       ))}
