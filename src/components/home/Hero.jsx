7953e89e4081f10275dd1407edf8f81aa93aed30
Dweina1920 <dina.yamanid@gmail.com>
2022-10-21 20:54:55 +0200

    first commit

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,22 +1,22 @@
-/* import React, { useState} from 'react'
-import axios from 'axios'
- */
-function Hero() {
-  const user = JSON.parse(localStorage.getItem("loggedUser"))
-  console.log(user)
+import React, { useState } from "react";
+import axios from "axios";
 
-  /*  const [ user, setUser ] = useState()
- 
-   const methodGet=async()=>{
-     await axios.get("http://localhost/proyectofinalBack/trabajadores.php")
-     .then(response=>{
-       setUser(response.data);
-     }).catch(error=>{
-       console.log(error);
-     })
-   }
- 
-   methodGet() */
+function Hero() {
+  /*const user = JSON.parse(localStorage.getItem("loggedUser"))
+  console.log(user)*/
+  const [user, setUser] = useState([]);
+  const methodGet = () => {
+    axios.get("https://fichajefactoria-default-rtdb.europe-west1.firebasedatabase.app/empleados.json")
+      .then((response) => {
+       
+        setUser(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  methodGet();
 
   return (
     <>
@@ -24,15 +24,13 @@ function Hero() {
         <h4 className="text-1xl font-bold self-center text-moradoFuerteF5">
           Hola..
         </h4>
-        <h2 className="text-3xl font-bold self-center text-moradoFuerteF5 ">
-          {user.name}
-        </h2>
+        <h2 className="text-3xl font-bold self-center text-moradoFuerteF5 ">carol</h2>
       </div>
 
 
       
     </>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
