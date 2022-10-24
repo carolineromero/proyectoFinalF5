8e555af588a2dffb5d7eb6b91a8de43014197ca8
@gilsusana <gilsusanaCIBAF5@gmail.com>
2022-10-24 14:28:59 +0200

    diseño home

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -7,16 +7,19 @@ function Hero() {
   return (
 
   <>
-    <div className="flex justify-center flex-col md ">
-      <h4 className="text-1xl font-bold self-center text-moradoFuerteF5">
-      👋 Hola..
-      </h4>  
-    <div className='flex justify-center flex-col '>
-      <h2 className="text-3xl font-bold mb-2 self-center text-moradoFuerteF5">
-      Isabel
-      </h2>
+  <div className="flex justify-center flex-col md ">
+    <h4 className="text-1xl font-bold self-center text-moradoFuerteF5">
+    👋 Hola..
+    </h4>  
+    <div className='flex justify-center flex-col justify-center'>
+          
+        <h2 className="text-3xl font-bold self-center text-moradoFuerteF5 z-20">
+          Carol
+        </h2>
+        <img src={rayarosa} alt="raya" rosa className='absolute self-center mt-1 h-2 opacity-1  w-28 z-0 opacity-30' />
+        </div>
     </div>
-    </div>
+
   </>
   )
 }
